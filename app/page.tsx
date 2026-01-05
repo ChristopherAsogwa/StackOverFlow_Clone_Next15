6afb097b3c8f71adcab354bbbce57da12fb6ed4b
ChristopherAsogwa <christopherasogwa8@gmail.com>
2026-01-05 08:07:23 +0100

    Customize tailwindcss theme to enhance style maintainability

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ const Home = () => {
   console.log("What type of component is this?");
   return (
     <div>
-      <h1 className="text-3xl font-bold underline">Welcome to my Stackoverflow Clone!</h1>
+      <h1 className="text-dark200_light900 h1-bold">Welcome to my Stackoverflow Clone!</h1>
     </div>
   );
 };
