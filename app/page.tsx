987e5b8646490591446418e3242e9c4905ddc9d3
ChristopherAsogwa <christopherasogwa8@gmail.com>
2026-01-05 07:37:45 +0100

    Set up ESLint and Prettier to enforce code quality and consistent formatting

--- app/page.tsx
+++ app/page.tsx
@@ -1,11 +1,10 @@
 const Home = () => {
-    console.log('What type of component is this?')
-    return (
-        <div>
-            <h1 className="text-3xl font-bold">
-                Welcome to my Stackoverflow Clone!
-            </h1>
-        </div>
-    )
-}
-export default Home
+  console.log("What type of component is this?");
+  return (
+    <div>
+      <h1 className="text-3xl font-bold underline">Welcome to my Stackoverflow Clone!</h1>
+    </div>
+  );
+};
+
+export default Home;
